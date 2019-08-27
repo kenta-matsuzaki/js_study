8e31d5623355eb305ab381f9c0e55bb19f69f9ca
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-27 14:08:43 +0900

    Update main.js

--- js_1/js1/main.js
+++ js_1/js1/main.js
@@ -3,9 +3,24 @@
 console.log('hello world!!!');
 console.log('helloooooooo');
 console.log('hey');
-
 console.log('it\'s mine')
-
 console.log("'hey'hey")
-
 console.log('hell\no wor\tld')
+
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+//定数と変数
+let price = 150;
+
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
+
+console.log(price * 140);
+console.log(price * 160);
