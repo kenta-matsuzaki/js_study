f42983906b4dad1ed1249e781f3cdb2b262ea6ad
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-27 14:02:30 +0900

    Summary(required)

---
+++ js_1/js1/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('hello world!!!');
+console.log('helloooooooo');
+console.log('hey');
+
+console.log('it\'s mine')
+
+console.log("'hey'hey")
+
+console.log('hell\no wor\tld')
