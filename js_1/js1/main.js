820875703c33ae6087c503e850c5f0c53e1deec1
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-27 14:11:29 +0900

    Update main.js

--- js_1/js1/main.js
+++ js_1/js1/main.js
@@ -15,12 +15,24 @@ console.log(10 % 3);
 console.log(10 ** 3);
 
 //定数と変数
-let price = 150;
+// let price = 150;
 
-console.log(price * 140);
-console.log(price * 160);
+// console.log(price * 140);
+// console.log(price * 160);
 
-price = 170;
+// price = 170;
 
-console.log(price * 140);
-console.log(price * 160);
+// console.log(price * 140);
+// console.log(price * 160);
+
+let price = 500
+
+price = price + 100;
+price += 100; //同上
+
+price *= 2 // price = price * 2
+
+price--; // price -= 1
+price++; //price += 1
+
+console.log(price);
