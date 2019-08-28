4644d7b367ec2e078d40105710e7c1b44afa18f3
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 10:25:57 +0900

    Update main.js

--- js_1/js1/main.js
+++ js_1/js1/main.js
@@ -136,12 +136,14 @@ function showAd(message = 'AD') { // 仮引数
   showAd('footer ad');
   showAd();
 
-  function sum(a, b, c) {
-    console.log(a + b + c);
-    // return a + b + c;
-  }
+  const sum = function sum(a, b, c) {
+    // console.log(a + b + c);
+    return a + b + c;
+  };
 
   // sum(1, 2, 3);
   // sum(3, 4, 5);
   const total = sum(1, 2, 3) + sum(3, 4, 5);
   console.log(total);
+
+  
