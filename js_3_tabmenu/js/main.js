3ff20b8e2eb2ba24f1bf39541cc5ab246afd0fef
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 22:23:14 +0900

    Update main.js

--- js_3_tabmenu/js/main.js
+++ js_3_tabmenu/js/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 {
-  
+  const menuItems = document.querySelectorAll('.menu li a');
+
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+    });
+  });
 }
