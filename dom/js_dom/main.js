75ef4c5b0c2e0f73d72dd7859d33d673a8df0eee
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 15:46:03 +0900

    Summary(required)

--- dom/js_dom/main.js
+++ dom/js_dom/main.js
@@ -3,11 +3,16 @@
 {
 // Document Object Model / DOM
 
-  document.querySelector('h1').textContent = 'H1!';
-  document.querySelectorAll('li:nth-child(odd)').forEach(li => { 
     // querySelectorAllはNodeListと呼ばれる配列と似たようなデータ構造になる
-    // nth-child(odd)で奇数番目だけを書き換えている
-    li.textContent = 'li!';
-  });
-  
+    // nth-child(odd)で奇数番目だけを書き換えることができる
+
+  const h1 = document.querySelector('h1');
+
+  console.log(h1.title);
+  h1.title = 'changed';
+
+  h1.style.color = 'red';
+  h1.style.backgroundColor = 'black';
+
+    
 }
