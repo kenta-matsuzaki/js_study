21ead72786a2f501d4b2fc460e942b83b110b7dc
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 15:54:48 +0900

    Summary(required)

--- dom/js_dom/main.js
+++ dom/js_dom/main.js
@@ -6,13 +6,7 @@
     // querySelectorAllはNodeListと呼ばれる配列と似たようなデータ構造になる
     // nth-child(odd)で奇数番目だけを書き換えることができる
 
-  const h1 = document.querySelector('h1');
-
-  console.log(h1.title);
-  h1.title = 'changed';
-
-  h1.style.color = 'red';
-  h1.style.backgroundColor = 'black';
-
+    const div = document.querySelector('div');
     
+    div.className = 'box border-pink';
 }
