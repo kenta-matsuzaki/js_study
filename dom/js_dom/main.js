370db70fdebd21fcbe7fdd00af985c20199451a7
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 16:38:32 +0900

    Summary(required)

--- dom/js_dom/main.js
+++ dom/js_dom/main.js
@@ -1,12 +1,21 @@
 'use strict';
 
 {
-// Document Object Model / DOM
 
-    // querySelectorAllはNodeListと呼ばれる配列と似たようなデータ構造になる
-    // nth-child(odd)で奇数番目だけを書き換えることができる
+  console.log(document.querySelectorAll('input[type="checkbox"]')[0].checked);
+  console.log(document.querySelectorAll('input[type="checkbox"]')[1].checked);
+  
+  document.querySelectorAll('input[type="checkbox"]')[0].checked = true;
 
-    const div = document.querySelector('div');
-    
-    div.className = 'box border-pink';
+  console.log(document.querySelectorAll('input[type="radio"]')[0].checked);
+  console.log(document.querySelectorAll('input[type="radio"]')[1].checked);
+  
+  document.querySelectorAll('input[type="radio"]')[1].checked = true;
+
+  console.log(document.querySelectorAll('select > option')[0].selected);
+  console.log(document.querySelectorAll('select > option')[1].selected);
+  console.log(document.querySelectorAll('select > option')[2].selected);
+
+  document.querySelectorAll('select > option')[2].selected = true;
+
 }
