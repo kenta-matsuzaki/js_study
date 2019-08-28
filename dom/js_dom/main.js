c0811e6bafb9dead5ef05b0e38af211024cc7397
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 15:32:27 +0900

    Summary(required)

--- dom/js_dom/main.js
+++ dom/js_dom/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
 {
+// Document Object Model / DOM
 
-  document.body.textContent = 'hello';
-
+  document.querySelector('h1').textContent = 'H1!';
+  document.querySelectorAll('li:nth-child(odd)').forEach(li => { 
+    // querySelectorAllはNodeListと呼ばれる配列と似たようなデータ構造になる
+    // nth-child(odd)で奇数番目だけを書き換えている
+    li.textContent = 'li!';
+  });
+  
 }
