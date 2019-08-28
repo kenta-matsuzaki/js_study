da092117b89efe725b9166970abb108e8109d946
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-28 11:01:20 +0900

    Update main.js

--- js_2/js2/main.js
+++ js_2/js2/main.js
@@ -2,11 +2,21 @@
 
 {
 // 配列
-  const scores = [80, 90, 40];
-  console.log(scores);
+  // const scores = [80, 90, 40];
+  // console.log(scores);
 
-  scores[2] = 100;
+  // scores[2] = 100;
 
-  console.log(scores);
-  console.log(scores.length);
+  // console.log(scores);
+  // console.log(scores.length);
+
+
+// オブジェクト
+  const player = {
+    name: 'matsuzaki',
+    score: 99,
+  };
+  
+  console.log(player);
+  
 }
