c0b513e34097cd433dc4556f52446357387f33ff
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-30 11:03:34 +0900

    Update main.js

--- js_6_treasureHunter/js/main.js
+++ js_6_treasureHunter/js/main.js
@@ -1,5 +1,17 @@
-'use strict';
+(function() {
+  'use strict';
 
-{
-  
-}
+  var boxes = document.getElementsByClassName('box');
+
+  function init() {
+    var i;
+    for (i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener('click', function() {
+        this.src = 'image/coin.png';
+        this.className = 'box';
+      });
+    }
+   }
+
+  init();
+})();
